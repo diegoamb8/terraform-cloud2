9aa28f2fc58256d26a52c2becff009e86f4ab06d
diegoamb8 <diegoalejandromartinez@unicesar.edu.co>
2024-10-05 17:21:52 -0500

    docker nginx

---
+++ command2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install docker -y
+sudo systemctl start docker 
+sudo usermod -a -G docker ec2-user
+sudo systemctl enable docker
+sudo docker run -d -p 80:80 --name nginx-container nginx
+sudo docker exec nginx-container /bin/bash -c "sed -i 's/<h1>Welcome to nginx!<\/h1>/<h1>Hola Diego Martinez - Nginx 2<\/h1>/g' /usr/share/nginx/html/index.html"
--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "ec2_pub1" {
-  ami                         = "ami-0d081196e3df05f4d"
+  ami                         = "ami-0fff1b9a61dec8a5f"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public_subnet_1.id
   key_name                    = "cloud2"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.allow_ssh_http.id]
+  user_data                   = file("command1.sh")
 
   tags = {
     Name = "pub1_ec2"
@@ -12,12 +13,13 @@ resource "aws_instance" "ec2_pub1" {
 }
 
 resource "aws_instance" "ec2_pub2" {
-  ami                         = "ami-0d081196e3df05f4d"
+  ami                         = "ami-0fff1b9a61dec8a5f"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public_subnet_2.id
   key_name                    = "cloud2"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.allow_ssh_http.id]
+  user_data                   = file("command2.sh")
 
   tags = {
     Name = "pub2_ec2"
